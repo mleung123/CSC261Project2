3d4a2cc2582a996b95183fcc8e8a12b157d4fb2f
Henry <goldhenr@grinnell.edu>
2025-04-02 14:21:17 -0500

    wrote episode driver

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::{collections::HashMap};
+use std::{collections::HashMap, ops::Neg};
 
-#[derive(PartialEq, Eq, Hash)]
+#[derive(PartialEq, Eq, Hash, Clone)]
 pub enum NegotiationMessage{
     Accept,
     Reject,
@@ -21,4 +21,26 @@ struct QLearning {
 
 fn main() {
     println!("Hello, world!");
+
+    driver();
+}
+
+fn driver(mut agent_1: impl RL, mut agent_2: impl RL, ep_num: u32) {
+
+    // Episode
+    let mut messages: Vec<NegotiationMessage> = Vec::new();
+    let mut last_message: NegotiationMessage = NegotiationMessage::Reject;
+    
+    while last_message != NegotiationMessage::Accept {
+
+        // Send message to agent 1
+        let offer: NegotiationMessage = agent_1.send(last_message);
+        messages.push(offer.clone());
+
+        // Check if offer is the end of the negotiation episode
+        if offer == NegotiationMessage::Accept { break }
+
+        last_message = agent_2.send(offer);
+        messages.push(last_message.clone());
+    }
 }
